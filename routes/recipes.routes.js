6f5c077afc726574f4548311663df0dbfe889011
Park Joyeong <drhamst@gmail.com>
2025-11-30 20:16:10 +0900

    운동 목록 조회

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -6,4 +6,8 @@ const recipesController = require("../controllers/recipes.controller");
 // GET /api/recipes?page=1&limit=20&recipe_title=검색어
 router.get("/", recipesController.getAllRecipes);
 
+// 운동 목록 조회 (공개 API - 로그인 불필요)
+// GET /api/recipes/:id
+router.get("/:id", recipesController.getRecipeExercises);
+
 module.exports = router;
